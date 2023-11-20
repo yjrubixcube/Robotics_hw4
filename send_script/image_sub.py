6edf65f26a83199f17a77c72105ad40a34a9fa89
User154742504910672534362390528 <jimmy960107@gmail.com>
2023-11-20 22:44:15 +0800

    這些是偷來的檔案XD

---
+++ send_script/image_sub.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import rclpy
+
+from rclpy.node import Node
+
+import sys
+sys.path.append('/home/robot/colcon_ws/install/tm_msgs/lib/python3.6/site-packages')
+from tm_msgs.msg import *
+from tm_msgs.srv import *
+
+from sensor_msgs.msg import Image
+
+
+class ImageSub(Node):
+    def __init__(self, nodeName):
+        super().__init__(nodeName)
+        self.subscription = self.create_subscription(Image, 
+        'techman_image', self.image_callback, 10)
+    
+    def image_callback(self, data):
+        self.get_logger().info('Received image')
+
+        # TODO (write your code here)
+
+def send_script(script):
+    arm_node = rclpy.create_node('arm')
+    arm_cli = arm_node.create_client(SendScript, 'send_script')
+
+    while not arm_cli.wait_for_service(timeout_sec=1.0):
+        arm_node.get_logger().info('service not availabe, waiting again...')
+
+    move_cmd = SendScript.Request()
+    move_cmd.script = script
+    arm_cli.call_async(move_cmd)
+    arm_node.destroy_node()
+
+def set_io(state):
+    gripper_node = rclpy.create_node('gripper')
+    gripper_cli = gripper_node.create_client(SetIO, 'set_io')
+
+    while not gripper_cli.wait_for_service(timeout_sec=1.0):
+        node.get_logger().info('service not availabe, waiting again...')
+    
+    io_cmd = SetIO.Request()
+    io_cmd.module = 1
+    io_cmd.type = 1
+    io_cmd.pin = 0
+    io_cmd.state = state
+    gripper_cli.call_async(io_cmd)
+    gripper_node.destroy_node()
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ImageSub('image_sub')
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
