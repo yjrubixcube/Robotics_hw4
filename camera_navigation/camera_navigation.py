3fdafc3b341f2354eade5a693b9e018c459e80e9
User154742504910672534362390528 <jimmy960107@gmail.com>
2023-11-28 00:47:45 +0800

    fixed bugs

--- camera_navigation/camera_navigation.py
+++ camera_navigation/camera_navigation.py
@@ -4,11 +4,29 @@ from numpy.linalg import inv
 
 x=0;y=0 # x,y are the pixel xy in the image
 
-camera_mtx=np.array(3,3) # get this from calibrate camera
+camera_mtx=np.ndarray((3,3)) # get this from calibrate camera
 
 camera_mtx_inv=inv(camera_mtx) # invesing the matrix is required (see the definition of camera matrix)
 
-line_vec=camera_mtx_inv@np.ndarray([x,y,1]) # 畫素x,y 只能解出一條射線，這是射線的向量方向（以相機座標系來看）
+line_vec=camera_mtx_inv@np.array([x,y,1]) # 畫素x,y 只能解出一條射線，這是射線的向量方向（以相機座標系來看）
 
-rotation_matrix=np.array(4,4) # I don't know the meaning of abc, so 
+# camera coordinate to robot base coordinate
+# you need to choose one of the following
+
+cam_x=0;cam_y=0;cam_z=0
+
+rotation_matrix=np.array([[0,0,0,cam_x],[0,0,0,cam_y],[0,0,0,cam_z],[0,0,0,1]])
+print(rotation_matrix)
+# abc = xyz euler
+
+# abc = xzy euler
+
+# abc = yxz euler
+
+# abc = yzx euler
+
+# abc = zyx euler
+
+# abc = zxy euler
+
 
